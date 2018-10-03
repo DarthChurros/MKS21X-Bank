965134a94285d7845398149d42c088f93aa1adbc
DarthChurros <tnarayan00@stuy.edu>
2018-10-03 14:46:16 -0400

    Added get methods, setPassword

--- BankAccount.java
+++ BankAccount.java
@@ -6,3 +6,15 @@ public class BankAccount {
   public BankAccount(double balance, int accountID, String password) {
     //initialize here
   }
+
+  public double getBalance() {
+    return balance;
+  }
+
+  public int getAccountID() {
+    return accountID;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
